19a9bc9fe2b73745b667c05b60d192cbb426ead3
Adriana Campo <adrianac@talentu.co>
2020-01-23 17:07:11 -0500

    Calling class and methods to get pay

--- pagar.php
+++ pagar.php
@@ -4,10 +4,19 @@ if(!isset($_POST['producto'], $_POST['precio'])){
     exit("Hubo un error");
 }
 
+use PayPal\Api\Payer;
+use PayPal\Api\item;
+
 require 'config.php';
+
 
 $producto = htmlspecialchars($_POST['producto']);
 $precio   = htmlspecialchars($_POST['precio']);
 $precio   = (int) $precio;//Float si deseo fracciones
 $envio    = 0;
 $total    = $precio + $envio;
+
+$compra = new Payer();
+$compra->setPaymentMethod('paypal');
+
+$articulo = new Item();
